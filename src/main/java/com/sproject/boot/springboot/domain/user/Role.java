e40db474943211a4f8efb899bf101de9ac1a8df0
Taein Jang <dev.taein@gmail.com>
2021-07-14 18:13:52 +0900

    gitignore 등록

---
+++ src/main/java/com/sproject/boot/springboot/domain/user/Role.java
@@ -0,0 +1,15 @@
+package com.sproject.boot.springboot.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반손님");
+
+    private final String key;
+    private final String title;
+}
